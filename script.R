2d9808141fae3e1cef9fc17c804f4a07bad89657
emmanuelhaye <emmanuel.haye@insee.fr>
2023-02-28 10:13:46 +0000

    library simples

--- script.R
+++ script.R
@@ -1,12 +1,10 @@
 
 
-if (!require("ggplot2")) install.packages("ggplot2")
-if (!require("stringr")) install.packages("stringr")
-if (!require("dplyr")) install.packages("dplyr")
-if (!require("tidyverse")) install.packages("tidyverse")
 
 
-library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(stringr)
 
 df <- readr::read_csv2(
   "individu_reg.csv",
